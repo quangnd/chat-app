c0dbde02df968a62049c6e2be407e6e77194cc3c
quangnd <quangnd.edu@gmail.com>
2017-01-30 13:42:52 +0700

    Update interface

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 var port = process.env.PORT || 3000;
-var app = express();
 
 app.use(express.static('public'));
 
